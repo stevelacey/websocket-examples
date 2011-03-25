26a77067d65e56a9b7d96a1a703b64f667ccb7a7
Steve Lacey <steve@stevelacey.net>
2011-03-25 01:06:05 +0000

    Initial websockets examples.

---
+++ chat/web/js/main.js
@@ -0,0 +1,49 @@
+function init() {
+  websocket = new WebSocket("ws://i7.stevelacey.net:8000");
+  websocket.onopen = function(event) { onOpen(event) };
+  websocket.onclose = function(event) { onClose(event) };
+  websocket.onmessage = function(event) { onMessage(event) };
+  websocket.onerror = function(event) { onError(event) };
+}
+
+function onOpen(event) {
+  writeToScreen("Connected to server");
+}
+
+function onClose(event) {
+  writeToScreen("Disconnected from server");
+}
+
+function onMessage(event) {
+  writeToScreen(event.data);
+  $("div").scrollTop($("div")[0].scrollHeight);
+}
+
+function onError(event) {
+  writeToScreen('<span style="color: red">Error:' + event.data + '</span>');
+}
+
+function doSend(message) {
+  websocket.send(message);
+}
+
+function writeToScreen(message) {
+  $('div').append($('<p/>', {text: message}));
+}
+
+$(document).ready(function() {
+  $('form').each(function() {
+    var message = $(this).find('input[type="text"]');
+
+    $(this).submit(function(event) {
+      doSend(message.val());
+      message.val(null);
+      message.focus();
+      event.preventDefault();
+    });
+
+    message.focus();
+  });
+});
+
+window.addEventListener("load", init, false);
