f07d8d1f3ac2e580d2b4df7665936cc73560a9a6
Steve Lacey <steve@stevelacey.net>
2011-03-26 21:27:27 +0000

    Refactored date server.

--- date/server/date.js
+++ date/server/date.js
@@ -3,38 +3,25 @@ var	ws = require("websocket-server"),
     port = 8082,
     socket;
 
-/**
- * Initialises server-side functionality
- */
-function init() {
-	// Initialise WebSocket server
-	//socket = ws.createServer({debug: true});
-	socket = ws.createServer();
+// Initialise WebSocket server
+socket = ws.createServer();
 
-	// On incoming connection from client
-	socket.on("connection", function(client) {
-		// Attempt to fix ECONNRESET errors
-		// This listener is being called without causing any crashes. Good!
-		client._req.socket.removeAllListeners("error");
-		client._req.socket.on("error", function(err) {
-			util.log("Socket error 1: " + err);
-		});
+// On incoming connection from client
+socket.on("connection", function(client) {
+  util.log("CONNECT: " + client.id);
 
-		util.log("CONNECT: "+client.id);
+  // On client disconnect
+  client.on("close", function(){
+    util.log("CLOSE: " + client.id);
+  });
+});
 
-		// On client disconnect
-		client.on("close", function(){
-			util.log("CLOSE: "+client.id);
-		});
-	});
+// Start listening for WebSocket connections
+socket.listen(port);
+util.log("Server listening on port " + port);
 
-	// Start listening for WebSocket connections
-	socket.listen(port);
-	util.log("Server listening on port " + port);
 
-  pushTime();
-};
-
+// Push out current server time every second
 function pushTime() {
   var now = new Date();
   socket.broadcast(now.toString());
@@ -42,5 +29,4 @@ function pushTime() {
   setTimeout(pushTime, 1000);
 }
 
-// Initialise the server-side functionality
-init();
+pushTime();
